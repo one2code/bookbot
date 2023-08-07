6e6dffde055601752dcb7bf1180b08459380d7b9
Travis Bennett <t_bennett_a@protonmail.com>
2023-08-07 11:46:24 -0400

    Code update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-
 def word_count(text):
     return len(text.split())
+
 
 def letter_count(text):
     text = text.lower()
@@ -14,11 +14,22 @@ def letter_count(text):
                 letter_counts[letter] = 1
     return letter_counts
 
-with open('./books/frankenstein.txt') as f:
-        file_contents = f.read()
+
+file_path = "./books/frankenstein.txt"
+
+with open(file_path) as f:
+    file_contents = f.read()
+
+words_counted = word_count(file_contents)
 
 counts = letter_count(file_contents)
 number_of_letters = letter_count(file_contents)
-print(number_of_letters)
-print(counts)
+char_counts = list(counts.items())
+char_counts.sort(key=lambda x: x[1], reverse=True)
 
+print(f"---Begin report of {file_path}---\n")
+
+print(f"{words_counted} words found in the document\n")
+for char, count in char_counts:
+    if char.isalpha():
+        print(f"The letter {char} was found {count} times")
